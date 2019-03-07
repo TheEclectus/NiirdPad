1827f02d9731eee0ee82d864fd42aeb737dc1889
TheEcliptic <TheEclectus#6425>
2019-03-06 23:33:53 -0500

    Added a panel to the leftmost side of the form, a status bar to the bottom, and removed the spacing on the horizontal layout.

--- NiirdPad/GeneratedFiles/ui_NiirdPad.h
+++ NiirdPad/GeneratedFiles/ui_NiirdPad.h
@@ -12,10 +12,12 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 #include "QSDLPanel.h"
 
@@ -33,15 +35,17 @@ public:
     QAction *actionExport_Project;
     QWidget *centralWidget;
     QHBoxLayout *horizontalLayout;
+    QFrame *frame;
     QSDLPanel *widget;
     QMenuBar *menuBar;
     QMenu *menuFile;
+    QStatusBar *statusBar;
 
     void setupUi(QMainWindow *NiirdPadClass)
     {
         if (NiirdPadClass->objectName().isEmpty())
             NiirdPadClass->setObjectName(QString::fromUtf8("NiirdPadClass"));
-        NiirdPadClass->resize(600, 400);
+        NiirdPadClass->resize(1043, 400);
         actionNew_Project = new QAction(NiirdPadClass);
         actionNew_Project->setObjectName(QString::fromUtf8("actionNew_Project"));
         actionNew_File = new QAction(NiirdPadClass);
@@ -59,21 +63,45 @@ public:
         centralWidget = new QWidget(NiirdPadClass);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         horizontalLayout = new QHBoxLayout(centralWidget);
-        horizontalLayout->setSpacing(6);
+        horizontalLayout->setSpacing(0);
         horizontalLayout->setContentsMargins(11, 11, 11, 11);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        frame = new QFrame(centralWidget);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(4);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(frame->sizePolicy().hasHeightForWidth());
+        frame->setSizePolicy(sizePolicy);
+        frame->setMinimumSize(QSize(200, 0));
+        frame->setMaximumSize(QSize(16777215, 16777215));
+        frame->setSizeIncrement(QSize(0, 0));
+        frame->setFrameShape(QFrame::Box);
+        frame->setFrameShadow(QFrame::Plain);
+
+        horizontalLayout->addWidget(frame);
+
         widget = new QSDLPanel(centralWidget);
         widget->setObjectName(QString::fromUtf8("widget"));
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(20);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
+        widget->setSizePolicy(sizePolicy1);
 
         horizontalLayout->addWidget(widget);
 
         NiirdPadClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(NiirdPadClass);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 600, 21));
+        menuBar->setGeometry(QRect(0, 0, 1043, 21));
         menuFile = new QMenu(menuBar);
         menuFile->setObjectName(QString::fromUtf8("menuFile"));
         NiirdPadClass->setMenuBar(menuBar);
+        statusBar = new QStatusBar(NiirdPadClass);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        NiirdPadClass->setStatusBar(statusBar);
 
         menuBar->addAction(menuFile->menuAction());
         menuFile->addAction(actionNew_Project);
