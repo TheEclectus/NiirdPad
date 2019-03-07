1ab8753a9f1f4af80e0de0daa28fef7d995ef66a
TheEcliptic <TheEclectus#6425>
2019-03-06 22:59:30 -0500

    Flavor changes to file menu.

--- NiirdPad/GeneratedFiles/ui_NiirdPad.h
+++ NiirdPad/GeneratedFiles/ui_NiirdPad.h
@@ -24,6 +24,13 @@ QT_BEGIN_NAMESPACE
 class Ui_NiirdPadClass
 {
 public:
+    QAction *actionNew_Project;
+    QAction *actionNew_File;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionSave_As;
+    QAction *actionImport_Project;
+    QAction *actionExport_Project;
     QWidget *centralWidget;
     QHBoxLayout *horizontalLayout;
     QSDLPanel *widget;
@@ -35,6 +42,20 @@ public:
         if (NiirdPadClass->objectName().isEmpty())
             NiirdPadClass->setObjectName(QString::fromUtf8("NiirdPadClass"));
         NiirdPadClass->resize(600, 400);
+        actionNew_Project = new QAction(NiirdPadClass);
+        actionNew_Project->setObjectName(QString::fromUtf8("actionNew_Project"));
+        actionNew_File = new QAction(NiirdPadClass);
+        actionNew_File->setObjectName(QString::fromUtf8("actionNew_File"));
+        actionOpen = new QAction(NiirdPadClass);
+        actionOpen->setObjectName(QString::fromUtf8("actionOpen"));
+        actionSave = new QAction(NiirdPadClass);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        actionSave_As = new QAction(NiirdPadClass);
+        actionSave_As->setObjectName(QString::fromUtf8("actionSave_As"));
+        actionImport_Project = new QAction(NiirdPadClass);
+        actionImport_Project->setObjectName(QString::fromUtf8("actionImport_Project"));
+        actionExport_Project = new QAction(NiirdPadClass);
+        actionExport_Project->setObjectName(QString::fromUtf8("actionExport_Project"));
         centralWidget = new QWidget(NiirdPadClass);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         horizontalLayout = new QHBoxLayout(centralWidget);
@@ -55,6 +76,14 @@ public:
         NiirdPadClass->setMenuBar(menuBar);
 
         menuBar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionNew_Project);
+        menuFile->addAction(actionOpen);
+        menuFile->addSeparator();
+        menuFile->addAction(actionSave);
+        menuFile->addAction(actionSave_As);
+        menuFile->addSeparator();
+        menuFile->addAction(actionImport_Project);
+        menuFile->addAction(actionExport_Project);
 
         retranslateUi(NiirdPadClass);
 
@@ -64,6 +93,13 @@ public:
     void retranslateUi(QMainWindow *NiirdPadClass)
     {
         NiirdPadClass->setWindowTitle(QApplication::translate("NiirdPadClass", "NiirdPad", nullptr));
+        actionNew_Project->setText(QApplication::translate("NiirdPadClass", "New", nullptr));
+        actionNew_File->setText(QApplication::translate("NiirdPadClass", "New File", nullptr));
+        actionOpen->setText(QApplication::translate("NiirdPadClass", "Open", nullptr));
+        actionSave->setText(QApplication::translate("NiirdPadClass", "Save", nullptr));
+        actionSave_As->setText(QApplication::translate("NiirdPadClass", "Save As...", nullptr));
+        actionImport_Project->setText(QApplication::translate("NiirdPadClass", "Import Project", nullptr));
+        actionExport_Project->setText(QApplication::translate("NiirdPadClass", "Export Project", nullptr));
         menuFile->setTitle(QApplication::translate("NiirdPadClass", "File", nullptr));
     } // retranslateUi
 
