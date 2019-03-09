712753456c4e344e95783fe4f4925423bacb0b1d
TheEcliptic <TheEclectus#6425>
2019-03-09 01:33:38 -0500

    Fixed a typo that as reading the wrong member of the window event as the type.

--- NiirdPad/QNodeView.cpp
+++ NiirdPad/QNodeView.cpp
@@ -8,13 +8,15 @@ void QNodeView::Input()
 		if (Event.type == SDL_EventType::SDL_WINDOWEVENT)
 		{
 			SDL_WindowEvent &WinEvent = Event.window;
-			if (WinEvent.type == SDL_WindowEventID::SDL_WINDOWEVENT_RESIZED)
+			if (WinEvent.event == SDL_WindowEventID::SDL_WINDOWEVENT_RESIZED)
 			{
 				int NewWidth = WinEvent.data1;
 				int NewHeight = WinEvent.data2;
 
 				_Camera.ViewBox.w = NewWidth;
 				_Camera.ViewBox.h = NewHeight;
+
+				printf_s("Camera adjusted to %dx%d.\n", _Camera.ViewBox.w, _Camera.ViewBox.h);
 			}
 		}
 	}
