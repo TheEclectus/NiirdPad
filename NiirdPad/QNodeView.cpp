1e4b65184f007b60a458ff203da5a6585b382e83
TheEcliptic <TheEclectus#6425>
2019-03-09 01:23:15 -0500

    Created QNodeViewCamera struct to contain information about what is visible in QNodeView. Added handling for SDL window resizing event, and adjusts data stored in _Camera.

--- NiirdPad/QNodeView.cpp
+++ NiirdPad/QNodeView.cpp
@@ -2,7 +2,22 @@
 
 void QNodeView::Input()
 {
+	SDL_Event Event;
+	while (SDL_PollEvent(&Event))
+	{
+		if (Event.type == SDL_EventType::SDL_WINDOWEVENT)
+		{
+			SDL_WindowEvent &WinEvent = Event.window;
+			if (WinEvent.type == SDL_WindowEventID::SDL_WINDOWEVENT_RESIZED)
+			{
+				int NewWidth = WinEvent.data1;
+				int NewHeight = WinEvent.data2;
 
+				_Camera.ViewBox.w = NewWidth;
+				_Camera.ViewBox.h = NewHeight;
+			}
+		}
+	}
 }
 
 void QNodeView::Logic()
--- NiirdPad/QNodeView.h
+++ NiirdPad/QNodeView.h
@@ -2,10 +2,17 @@
 
 #include "QSDLPanel.h"
 
+struct QNodeViewCamera
+{
+	SDL_Rect ViewBox;	// Center position and size.
+	float Scale = 1.0f;
+};
+
 class QNodeView : public QSDLPanel
 {
 protected:
 	SDL_Color _GridLineColor = { 0x1E, 0x1E, 0x1E, 0xFF };
+	QNodeViewCamera _Camera;
 
 	virtual void Input() override;
 	virtual void Logic() override;
