b51e12955f073449e9fe98a81e2ad6837729c080
TheEcliptic <TheEclectus#6425>
2019-03-22 22:33:43 -0400

    Started implementing AGraphicsBlock. Added SDL_FontCache.

--- NiirdPad/GraphicsBlocks.h
+++ NiirdPad/GraphicsBlocks.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <SDL_rect.h>
+
+/// <summary>
+/// A graphical building block class. Has faculties for defining the bounding box
+/// of a graphical object based on its own criteria, and that of blocks within it.
+/// </summary>
+class AGraphicsBlock
+{
+protected:
+	/// <summary>
+	/// Defines the maximum size of the block. {-1, -1} means no limit.
+	/// </summary>
+	SDL_Rect _MaximumSize = { -1, -1 };
+	/// <summary>
+	/// Defines the minimum size of a block.
+	/// </summary>
+	SDL_Rect _MinimumSize = { 0, 0 };
+public:
+	void CalculateSize();
+};
