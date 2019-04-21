7ff6dc4540b438928e17e417e7338d0bb5315605
TheEcliptic <TheEclectus#6425>
2019-04-21 18:12:46 -0400

    Began defining the Node class, to represent the non-graphical information of a single Node.

--- NiirdPad/Node.cpp
+++ NiirdPad/Node.cpp
@@ -2,7 +2,7 @@
 
 #include "GraphicsBlocks.h"
 
-NodeInputBox::NodeInputBox(SDL_Renderer *Renderer, const std::string &Reference, const std::string &Script, const std::string &Dialogue)
+NodeDialogue::NodeDialogue(SDL_Renderer *Renderer, const std::string &Reference, const std::string &Script, const std::string &Dialogue)
 {
 	//_graphics = new GraphicsBlock_NodeInputBox(Renderer, )
 }
--- NiirdPad/Node.h
+++ NiirdPad/Node.h
@@ -7,18 +7,38 @@
 // GraphicsBlocks.h
 class GraphicsBlock_NodeInputBox;
 class GraphicsBlock_NodeOutputBox;
+class GraphicsBlock_Node;
 
-class NodeInputBox
+class NodeDialogue
 {
 private:
 	GraphicsBlock_NodeInputBox *_graphics;
 
 	std::string _reference, _script, _dialogue;
 public:
-	NodeInputBox(SDL_Renderer *Renderer, const std::string &Reference, const std::string &Script = "", const std::string &Dialogue = "");
+	NodeDialogue(SDL_Renderer *Renderer, const std::string &Reference, const std::string &Script = "", const std::string &Dialogue = "");
 
 	void SetAll(const std::string &Reference, std::string &Script, const std::string &Dialogue);
 	void SetReference(const std::string &Reference);
 	void SetScriptLines(const std::string &Script);
 	void SetDialogue(const std::string &Dialogue);
+
+	GraphicsBlock_NodeInputBox *Graphics();
+};
+
+class Node
+{
+private:
+	GraphicsBlock_Node *_graphics;
+	std::vector<NodeDialogue*> _dialogues;
+
+public:
+	Node();
+	~Node();
+
+	GraphicsBlock_Node &Graphics();
+
+	NodeDialogue *AddDialogue();
+	void RemoveDialogue(NodeDialogue *Dlg);
+	const std::vector<const NodeDialogue *const> &Dialogues() const;
 };
