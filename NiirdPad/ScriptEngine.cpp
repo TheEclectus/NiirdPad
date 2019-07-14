ece7af50523c72923ebb31eadb9250fe5522ca9b
TheEcliptic <TheEclectus#6425>
2019-07-14 12:52:05 -0400

    Forgot to commit this last time.

---
+++ NiirdPad/ScriptEngine.cpp
@@ -0,0 +1,2 @@
+#include "ScriptEngine.h"
+
--- NiirdPad/ScriptEngine.h
+++ NiirdPad/ScriptEngine.h
@@ -4,20 +4,23 @@
 /// What it is:
 /// - A class to verify individual script strings with a function call and return success information
 /// What it is not:
-/// - Owning of scripts
+/// - Owning of TUScript scripts
 /// </summary>
 
 #define SOL_ALL_SAFETIES_ON 1
 #include <sol/sol.hpp>
 
+#include <unordered_map>
 #include <vector>
 
 class ScriptEngine
 {
 private:
 	sol::state _state;
+	std::unordered_map<std::string, sol::function> _validationFunctions;
 public:
 	ScriptEngine();
+
 	/// <returns>
 	/// True on success, false on error; ConnectionKeys will be filled with any connection options
 	/// provided by the function 
