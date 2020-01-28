82782d9cb225bd629e02a99e5dea528982bcae0d
TheEcliptic <TheEclectus#6425>
2020-01-28 18:53:47 -0500

    Re-implemented tri-state checkbox feature from Qt for QTreeWidget because successive sets would not trigger an itemChanged() signal.

--- NiirdPad/QCharacterSelectionWindow.cpp
+++ NiirdPad/QCharacterSelectionWindow.cpp
@@ -1,11 +1,151 @@
 #include "QCharacterSelectionWindow.h"
 
+void QCharacterSelectionWindow::SetCheckState(QTreeWidgetItem *Item, Qt::CheckState State)
+{
+	Item->treeWidget()->blockSignals(true);
+
+	if(Item->checkState(0) != State)
+		Item->setCheckState(0, State);
+
+	Item->treeWidget()->blockSignals(false);
+}
+
+QTreeWidgetItem *FindTLInTree(QTreeWidget *Widget, QString &Label)
+{
+	for (int i = 0; i < Widget->topLevelItemCount(); i++)
+	{
+		if (Widget->topLevelItem(i)->text(0) == Label)
+			return Widget->topLevelItem(i);
+	}
+
+	return nullptr;
+}
+
+QTreeWidgetItem *FindItemInTL(QTreeWidgetItem *TL, QString &Label)
+{
+	for (int i = 0; i < TL->childCount(); i++)
+	{
+		if (TL->child(i)->text(0) == Label)
+			return TL->child(i);
+	}
+
+	return nullptr;
+}
+
+void ParentLogic(QTreeWidget *Existing, QTreeWidgetItem *Item, Qt::CheckState State)
+{
+	auto *PairedSideTL = FindTLInTree(Existing, Item->text(0));
+	if (PairedSideTL != nullptr)
+	{
+		QFont ItemFont = PairedSideTL->font(0);
+		switch (State)
+		{
+		case Qt::CheckState::Checked:
+			ItemFont.setItalic(true);
+			ItemFont.setStrikeOut(true);
+			break;
+		case Qt::CheckState::PartiallyChecked:
+			ItemFont.setItalic(true);
+			ItemFont.setStrikeOut(false);
+			break;
+		case Qt::CheckState::Unchecked:
+			ItemFont.setItalic(false);
+			ItemFont.setStrikeOut(false);
+			break;
+		}
+		PairedSideTL->setFont(0, ItemFont);
+	}
+}
+
+void ChildLogic(QTreeWidget *Existing, QTreeWidgetItem *Item, Qt::CheckState State)
+{
+	QTreeWidgetItem *ParentItem = Item->parent();
+
+	auto *PairedSideTL = FindTLInTree(Existing, ParentItem->text(0));
+	if (PairedSideTL != nullptr)
+	{
+		// Get the matching child item on the other side
+		auto *PairedSideChild = FindItemInTL(PairedSideTL, Item->text(0));
+		if (PairedSideChild != nullptr)
+		{
+			QFont ItemFont = PairedSideChild->font(0);
+			switch (State)
+			{
+			case Qt::CheckState::Checked:
+				ItemFont.setItalic(true);
+				ItemFont.setStrikeOut(true);
+				break;
+			case Qt::CheckState::Unchecked:
+				ItemFont.setItalic(false);
+				ItemFont.setStrikeOut(false);
+				break;
+			}
+			PairedSideChild->setFont(0, ItemFont);
+		}
+	}
+}
+
+void QCharacterSelectionWindow::ProcessCheck(QTreeWidgetItem *Item, Qt::CheckState State)
+{
+	// Set the check state
+	SetCheckState(Item, State);
+
+	// Top-level items
+	if (Item->parent() == nullptr)
+	{
+		// Parent logic here
+		ParentLogic(ui.treeExisting, Item, State);
+
+		// Iterate through children and set their states accordingly.
+		for (int i = 0; i < Item->childCount(); i++)
+		{
+			auto *ChildItem = Item->child(i);
+			SetCheckState(ChildItem, State);
+
+			// Apply logic here
+			ChildLogic(ui.treeExisting, ChildItem, State);
+		}
+	}
+	else // Child items
+	{
+		QTreeWidgetItem *ParentItem = Item->parent();
+		
+		SetCheckState(Item, State);
+		// Apply logic here
+		ChildLogic(ui.treeExisting, Item, State);
+
+		int CheckedItems = 0;
+		// Iterate through children and set their states accordingly.
+		for (int i = 0; i < ParentItem->childCount(); i++)
+		{
+			//SetCheckState(ParentItem->child(i), State);
+			if(ParentItem->child(i)->checkState(0) == Qt::CheckState::Checked)
+				CheckedItems++;
+		}
+
+		Qt::CheckState ParentCheckState = Qt::CheckState::Unchecked;
+		if (CheckedItems > 0 && CheckedItems < ParentItem->childCount())
+		{
+			ParentCheckState = Qt::CheckState::PartiallyChecked;
+		}
+		else if(CheckedItems == ParentItem->childCount())
+		{
+			ParentCheckState = Qt::CheckState::Checked;
+		}
+
+		SetCheckState(ParentItem, ParentCheckState);
+		ParentLogic(ui.treeExisting, ParentItem, ParentCheckState);
+	}
+}
+
 QCharacterSelectionWindow::QCharacterSelectionWindow(QWidget *parent) :
 	QDialog(parent)
 {
 	ui.setupUi(this);
 
 	ui.treeSelection->setHeaderLabel("diag.txt (static text)");
+	ui.treeExisting->setHeaderLabel("Untitled Project");
+	//ui.treeExisting->setHeaderHidden(true);
 
 	connect(ui.btnSelection_SelectAll, &QPushButton::pressed, [this]() {
 		for (int i = 0; i < this->ui.treeSelection->topLevelItemCount(); i++)
@@ -20,10 +160,14 @@ QCharacterSelectionWindow::QCharacterSelectionWindow(QWidget *parent) :
 		{
 			auto x = this->ui.treeSelection->topLevelItem(i);
 			x->setCheckState(0, Qt::CheckState::Unchecked);
+
+		//	this->UncheckTreeWidgetItem(this->ui.treeSelection->topLevelItem(i));
 		}
 		//this->ui.treeSelection->clearSelection();
 	});
 	connect(ui.btnExisting_SelectAll, &QPushButton::pressed, [this]() {
+		return;
+		
 		for (int i = 0; i < this->ui.treeExisting->topLevelItemCount(); i++)
 		{
 			auto x = this->ui.treeExisting->topLevelItem(i);
@@ -32,6 +176,8 @@ QCharacterSelectionWindow::QCharacterSelectionWindow(QWidget *parent) :
 		//this->ui.treeExisting->selectAll();
 	});
 	connect(ui.btnExisting_DeselectAll, &QPushButton::pressed, [this]() {
+		return;
+		
 		for (int i = 0; i < this->ui.treeExisting->topLevelItemCount(); i++)
 		{
 			auto x = this->ui.treeExisting->topLevelItem(i);
@@ -40,71 +186,104 @@ QCharacterSelectionWindow::QCharacterSelectionWindow(QWidget *parent) :
 		//this->ui.treeExisting->clearSelection();
 	});
 	connect(ui.treeSelection, &QTreeWidget::itemChanged, [this](QTreeWidgetItem *Item, int Column) {
-		if (Item->parent() == nullptr)
-		{
-			// Routine for a top-level item.
+		
+		ProcessCheck(Item, Item->checkState(0));
 
-			QTreeWidgetItem *FoundTLItem = nullptr;
-			QString ItemText = Item->text(0);
-			int ExistingTopLevelItems = ui.treeExisting->topLevelItemCount();
-			for (int i = 0; i < ExistingTopLevelItems; i++)
-			{
-				auto *CurTLItem = ui.treeExisting->topLevelItem(i);
-				if (CurTLItem->text(0) == ItemText)
-				{
-					FoundTLItem = CurTLItem;
-					break;
-				}
-			}
+		//if (Item->parent() == nullptr)
+		//{
+		//	// Routine for a top-level item.
 
-			if (FoundTLItem == nullptr)
-				return;
-			else
-			{
-				QFont ItemFont = FoundTLItem->font(0);
-				switch (Item->checkState(0))
-				{
-				case Qt::CheckState::Checked:
-					ItemFont.setStrikeOut(true);
-				case Qt::CheckState::PartiallyChecked:
-					ItemFont.setItalic(true);
-					break;
-				case Qt::CheckState::Unchecked:
-					ItemFont.setStrikeOut(false);
-					ItemFont.setItalic(false);
-					break;
-				}
+		//	QTreeWidgetItem *FoundTLItem = nullptr;
+		//	QString ItemText = Item->text(0);
+		//	int ExistingTopLevelItems = ui.treeExisting->topLevelItemCount();
+		//	for (int i = 0; i < ExistingTopLevelItems; i++)
+		//	{
+		//		auto *CurTLItem = ui.treeExisting->topLevelItem(i);
+		//		if (CurTLItem->text(0) == ItemText)
+		//		{
+		//			FoundTLItem = CurTLItem;
+		//			break;
+		//		}
+		//	}
 
-				FoundTLItem->setFont(0, ItemFont);
-			}
-		}
-		else
-		{
-			// Child item
-			QString ItemText = Item->text(0);
-			QString ParentText = Item->parent()->text(0);
+		//	if (FoundTLItem == nullptr)
+		//		return;
 
-			// Existing side item
-			QTreeWidgetItem *FoundTLItem = nullptr;
-			int ExistingTopLevelItems = ui.treeExisting->topLevelItemCount();
-			for (int i = 0; i < ExistingTopLevelItems; i++)
-			{
-				auto *CurTLItem = ui.treeExisting->topLevelItem(i);
-				if (CurTLItem->text(0) == ParentText)
-				{
-					FoundTLItem = CurTLItem;
-					break;
-				}
-			}
-		}
+		//	QFont ItemFont = FoundTLItem->font(0);
+		//	switch (Item->checkState(0))
+		//	{
+		//	case Qt::CheckState::Checked:
+		//		ItemFont.setStrikeOut(true);
+		//	case Qt::CheckState::PartiallyChecked:
+		//		ItemFont.setItalic(true);
+		//		break;
+		//	case Qt::CheckState::Unchecked:
+		//		ItemFont.setStrikeOut(false);
+		//		ItemFont.setItalic(false);
+		//		break;
+		//	}
+
+		//	FoundTLItem->setFont(0, ItemFont);
+
+		//	// Now iterate through all the top-level's children and set them to the same state.
+		//}
+		//else
+		//{
+		//	// Child item
+		//	QString ItemText = Item->text(0);
+		//	QString ParentText = Item->parent()->text(0);
+
+		//	// Existing side item
+		//	QTreeWidgetItem *FoundTLItem = nullptr;
+		//	int ExistingTopLevelItems = ui.treeExisting->topLevelItemCount();
+		//	for (int i = 0; i < ExistingTopLevelItems; i++)
+		//	{
+		//		auto *CurTLItem = ui.treeExisting->topLevelItem(i);
+		//		auto CurTLItemText = CurTLItem->text(0);
+		//		if (CurTLItemText == ParentText)
+		//		{
+		//			FoundTLItem = CurTLItem;
+		//			break;
+		//		}
+		//	}
+
+		//	if (FoundTLItem != nullptr)
+		//	{
+		//		QTreeWidgetItem *FoundChildItem = nullptr;
+		//		for (int i = 0; i < FoundTLItem->childCount(); i++)
+		//		{
+		//			FoundChildItem = FoundTLItem->child(i);
+		//			auto FoundChildItemText = FoundChildItem->text(0);
+		//			if (FoundChildItemText == ItemText)
+		//			{
+		//				QFont ItemFont = FoundChildItem->font(0);
+		//				switch (Item->checkState(0))
+		//				{
+		//				case Qt::CheckState::Checked:
+		//					ItemFont.setStrikeOut(true);
+		//					ItemFont.setItalic(true);
+		//					break;
+		//				case Qt::CheckState::Unchecked:
+		//					ItemFont.setStrikeOut(false);
+		//					ItemFont.setItalic(false);
+		//					break;
+		//				}
+		//				FoundChildItem->setFont(0, ItemFont);
+		//				break;
+		//			}
+		//		}
+		//	}
+		//}
+
+		//ui.treeSelection->blockSignals(false);
 	});
 
 	QList<QTreeWidgetItem*> Items;
 	for (int i = 0; i < 10; i++)
 	{
 		auto NewItem = new QTreeWidgetItem((QTreeWidget*)nullptr, QStringList(QString("Character %1").arg(i)));
-		NewItem->setCheckState(0, Qt::CheckState::PartiallyChecked);
-		NewItem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsAutoTristate);
+		NewItem->setCheckState(0, Qt::CheckState::Unchecked);
+		NewItem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled /*| Qt::ItemIsAutoTristate*/);
 
 		for (int x = 0; x < 3; x++)
 		{
--- NiirdPad/QCharacterSelectionWindow.h
+++ NiirdPad/QCharacterSelectionWindow.h
@@ -10,6 +10,29 @@ class QCharacterSelectionWindow : public QDialog
 private:
 	Ui::QCharacterSelectionWindow ui;
 
+	/*
+		CUSTOM TRI-STATE IMPLEMENTATION
+
+		Whenever an itme is clicked:
+		1. If it doesn't match the desired state, set it.
+		2. If it's a top-level item...
+			1. Modify all child widgets to match its state without affecting its parent.
+		3. If it's a child item...
+			1. Alter the parent item without affecting the children.
+				If all children are checked, check.
+				If some children are checked, half-check.
+				If no children are checked, uncheck.
+	*/
+
+	// Just sets the check state.
+	void SetCheckState(QTreeWidgetItem *Item, Qt::CheckState State);
+	void ProcessCheck(QTreeWidgetItem *Item, Qt::CheckState State);
+
+	//QTreeWidgetItem *FindTLInTree(QTreeWidget *Widget, QString &Label);
+	//QTreeWidgetItem *FindItemInTL(QTreeWidgetItem *TL, QString &Label);
+
+	//void UncheckTreeWidgetItem(QTreeWidgetItem *Item) const;
+
 public:
 	QCharacterSelectionWindow(QWidget *parent = Q_NULLPTR);
 	~QCharacterSelectionWindow();
